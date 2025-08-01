a224bbce850986baaa112418c230cbc9664ef054
hasnain raza <hasnainraza116@gmail.com>
2025-08-01 16:55:59 +0100

    test

--- app.py
+++ app.py
@@ -28,5 +28,4 @@ def predict():
     return render_template('index.html', prediction_text='Prediction: {}'.format(output))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
-    #app.run(debug=True)
+    app.run(debug=True)
